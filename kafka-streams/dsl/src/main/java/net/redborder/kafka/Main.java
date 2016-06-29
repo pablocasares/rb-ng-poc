07942089878e6fa2988d128b93f2092e5c7aee44
Pablo Casares <pcasares@redborder.net>
2016-06-29 10:43:56 +0200

    Fixed FlowReputationJoiner test

--- kafka-streams/dsl/src/main/java/net/redborder/kafka/Main.java
+++ kafka-streams/dsl/src/main/java/net/redborder/kafka/Main.java
@@ -8,7 +8,6 @@ public class Main {
 
     public static void main(String args[]) {
 
-
         try {
             logger.info("Starting location enrichment");
             RedborderProcessor.process();
--- kafka-streams/dsl/src/test/net/redborder/kafka/test/unit/FlowReputationJoiner.java
+++ kafka-streams/dsl/src/test/net/redborder/kafka/test/unit/FlowReputationJoiner.java
@@ -18,13 +18,12 @@ public class FlowReputationJoiner {
 
         // Input 1: Flow Data
         Map<String, Object> flow_data = objectMapper.readValue("{\"application\":\"application_A\",\"bytes\":123456789,\"client\":\"client_A\",\"ip\":\"10.0.0.2\"}", Map.class);
-        Map<String, Object> location_data = objectMapper.readValue("{\"ip\":\"10.0.0.2\",\"score\":80}", Map.class);
+        Map<String, Object> reputation_data = objectMapper.readValue("{\"ip\":\"10.0.0.2\",\"score\":80}", Map.class);
 
-        Map<String, Object> actualEnrichment = new net.redborder.kafka.FlowLocationJoiner().apply(flow_data, location_data);
+        Map<String, Object> actualEnrichment = new net.redborder.kafka.FlowReputationJoiner().apply(flow_data, reputation_data);
 
         // Expected data
         Map<String, Object> expectedEnrichment = objectMapper.readValue("{\"client\":\"client_A\",\"application\":\"application_A\",\"bytes\":123456789,\"ip\":\"10.0.0.2\",\"score\":80}", Map.class);
-
         assertThat(actualEnrichment, equalTo(expectedEnrichment));
     }
 
